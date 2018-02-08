5872bf9a85fe700d5d7789fb33ffe73edb0c5011
BoLu2019 <blu4@stuy.edu>
2018-02-08 09:24:10 -0500

    added driver

---
+++ MFDriver.java
@@ -0,0 +1,37 @@
+public class MFDriver{
+
+    public static int[][] generate(int size){
+	int[][] rtr = new int[size][size];
+	int increment = 0;
+	for(int x = 0; x< size; x++){
+	    int num = increment;
+	    for(int y = 0; y<size; y++){
+		num++;
+		rtr[x][y] = num;
+	    }
+	    increment++;	    
+	}
+
+
+	return
+    }
+
+    public static void main(String[] args){
+	int counter = 10000;
+	while(counter>0){
+
+	    MatrixFinder.finder
+
+	    counter--;
+
+	    
+
+	}
+
+
+	
+
+    }
+
+
+}

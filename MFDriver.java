4840e127bf7c291a69b7f8f91744ce35aa15fd61
rlin6 <rlin6@stuy.edu>
2018-02-08 22:59:17 -0500

    Fix data search

--- MFDriver.java
+++ MFDriver.java
@@ -15,12 +15,12 @@ public class MFDriver{
 	int size = 1; 
 	
 	
-	while(size <= 5000){
+	while(size <= 100){
 	    long total = 0;
 	    int counter = 0;
-	    a = new int[size][size];
+	    a = new int[size*100][size*100];
 	    populate(a);
-	    int target = (int) Math.random() * size;
+	    int target = (int) Math.random() * size * 100;
 
 	    while (counter < 10000) {
 		long startTime = System.nanoTime();
@@ -31,7 +31,7 @@ public class MFDriver{
 		total += endTime;
 		counter ++; 
 	    }
-	    System.out.println(size + " ," + total);
+	    System.out.println((size*100) + " ," + (total/10000));
 	    total = 0; 
 	    size++; 
 	} 
